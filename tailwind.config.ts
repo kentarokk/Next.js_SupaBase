091ff3d151e6bafc40e35467843f0bb788c19a99
kentarokk <ken.k.jul2@gmail.com>
2024-10-27 20:33:50 +0900

    end tutorial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -78,3 +78,19 @@ const config = {
 } satisfies Config;
 
 export default config;
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "black-rgba": "rgba(0, 0, 0, 0.3)",
+      },
+    },
+  },
+  plugins: [],
+};
